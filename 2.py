d3b02f38d541f9ea08fd97fff1f9da519aade9ce
modeverv <modeverv@gmail.com>
2017-06-13 23:34:33 +0900

    880

--- 2.py
+++ 2.py
@@ -11,7 +11,7 @@ def AND(x1, x2):
 	elif tmp > theta:
 		return 1
 
-
+print("AND")
 print(AND(0, 0))
 print(AND(1, 0))
 print(AND(0, 1))
@@ -28,29 +28,44 @@ def AND(x1, x2):
 	else:
 		return 1
 
-
+print("AND2")
 print(AND(0, 0))
 print(AND(1, 0))
 print(AND(0, 1))
 print(AND(1, 1))
 
-def NAND(x1,x2):
-	x = np.array([x1,x2])
-	w = np.array([-0.5,-0.5])
+
+def NAND(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([-0.5, -0.5])
 	b = 0.7
-	tmp = np.sum(w*x) + b
+	tmp = np.sum(w * x) + b
 	if tmp <= 0:
 		return 0
 	else:
 		return 1
 
-def OR(x1,x2):
-	x = np.array([x1,x2])
-	w = np.array([0.5,0.5])
+
+def OR(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
 	b = -0.2
-	tmp = np.sum(w*x) + b
+	tmp = np.sum(w * x) + b
 	if tmp <= 0:
 		return 0
 	else:
 		return 1
+
+
+def XOR(x1, x2):
+	s1 = NAND(x1, x2)
+	s2 = OR(x1, x2)
+	y = AND(s1, s2)
+	return y
+
+print("XOR")
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
 
