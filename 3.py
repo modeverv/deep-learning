95b880bf45431994cbfdd5b8e740130658afefc0
modeverv <modeverv@gmail.com>
2017-06-14 00:29:02 +0900

    1238

--- 3.py
+++ 3.py
@@ -9,8 +9,25 @@ def step_function(x):
 x = np.arange(-5.0,5.0,0.1)
 y = step_function(x)
 
-plt.plot(x,y)
-plt.ylim(-0.1,1.1)
-plt.show()
+#plt.plot(x,y)
+#plt.ylim(-0.1,1.1)
+#plt.show()
 
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+y = sigmoid(x)
+
+#plt.plot(x,y)
+#plt.ylim(-0.1,1.1)
+#plt.show()
+
+def xxx(x):
+	return x * x * x + 2 * x + 1
+
+y = xxx(x)
+
+plt.plot(x,y)
+plt.ylim(-500.1,500.1)
+plt.show()
 
