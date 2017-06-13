1f99db4c5801c0e60e1c03bc5abac77fc949db05
modeverv <modeverv@gmail.com>
2017-06-13 23:20:43 +0900

    830

--- 1.py
+++ 1.py
@@ -4,15 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # データの作成
-#x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
-#y = np.sin(x)
+# x = np.arange(0, 6, 0.1) # 0から6まで0.1刻みで生成
+# y = np.sin(x)
 
 # グラフの描画
-#plt.plot(x, y)
-#plt.show()
+# plt.plot(x, y)
+# plt.show()
 
 # リスト
-a = [1,2,3,4,5]
+a = [1, 2, 3, 4, 5]
 print(a[2])
 print(a[0:2])
 
@@ -28,33 +28,37 @@ sleepy = False
 
 # if !とか使えないのね notねあとelif
 if not hungry:
-  print("I'm hungry")
+	print("I'm hungry")
 elif not sleepy:
-  print("I'm sleepy")
+	print("I'm sleepy")
 else:
-  print("I'm not hungry")
+	print("I'm not hungry")
 
 # for
-for i in [1,2,3]:
-  print(i)
+for i in [1, 2, 3]:
+	print(i)
+
 
 # 関数
 def hello():
-  print("Hello World")
+	print("Hello World")
+
 
 hello()
 
+
 # クラス
 class Man:
-  def __init__(self,name):
-    self.name = name
-    print("Initialized!")
+	def __init__(self, name):
+		self.name = name
+		print("Initialized!")
 
-  def hello(self):
-    print("Hello " + self.name + "!")
+	def hello(self):
+		print("Hello " + self.name + "!")
 
-  def goodbye(self):
-    print("Good bye " + self.name + "!")
+	def goodbye(self):
+		print("Good bye " + self.name + "!")
+
 
 m = Man("nora inu")
 m.hello()
@@ -75,35 +79,27 @@ print(z)
 A = np.array([[1, 2], [3, 4]])
 print(A)
 print(A.shape)
-B = np.array([[3,0], [0,6]])
+B = np.array([[3, 0], [0, 6]])
 
 print(A + B)
 print(A * B)
 
-X = np.array([[51,55],[14,19],[0,4]])
+X = np.array([[51, 55], [14, 19], [0, 4]])
 
-print(X[X>15])
+print(X[X > 15])
 
-x = np.arange(0,6,0.1)
+x = np.arange(0, 6, 0.1)
 y = np.sin(x)
-#plt.plot(x,y)
-#plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 y1 = np.sin(x)
 y2 = np.cos(x)
 
-plt.plot(x,y1,label ="sin")
-plt.plot(x,y2,linestyle="--",label="cos")
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("sin & cos")
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
