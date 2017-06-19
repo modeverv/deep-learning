94eaa1138de9d79e54d61b617050e23ece4a9891
modeverv <modeverv@gmail.com>
2017-06-20 01:10:29 +0900

    3575

--- 5.4.1.py
+++ 5.4.1.py
@@ -57,3 +57,35 @@ print(price)
 print(dapple_num,dapple)
 print(dorange_num,dorange)
 print(dtax)
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self,x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self,dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+# http://nonbiri-tereka.hatenablog.com/entry/2014/06/30/134023
+import numpy as np
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self,x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self,dout):
+        dx = dout * (1.0 - self.out) * self.out
+
